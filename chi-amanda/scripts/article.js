3c6f1e846f5d36fa16c11c6ddb454c67caeb0a51
AmandaMoen <amandalmoen@yahoo.com>
2018-06-12 13:24:20 -0700

    Completed our README.md

--- chi-amanda/scripts/article.js
+++ chi-amanda/scripts/article.js
@@ -33,7 +33,7 @@ Article.prototype.toHtml = function () {
 // REVIEW: This function will take the rawData, how ever it is provided, and use it to instantiate all the articles. This code is moved from elsewhere, and encapsulated in a simply-named function for clarity.
 
 // COMMENT: Where is this function called? What does 'rawData' represent now? How is this different from previous labs?
-// This function is called in Article.fetchAll.
+// This function is called in Article.fetchAll. rawData represents the data in local storage. In previous labs the rawData had it's own JS file, in this lab the raw data starts in a JSON file.
 Article.loadAll = articleData => {
   articleData.sort((a, b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
 
